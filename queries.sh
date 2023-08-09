66f57511317f43bd193e4a7074dea1af5a78cb2f
brodam26 <53849737+brodam26@users.noreply.github.com>
2023-08-09 15:25:10 -0500

    Update queries.sh

--- queries.sh
+++ queries.sh
@@ -21,7 +21,7 @@ echo "$($PSQL "SELECT ROUND(AVG(winner_goals), 2) FROM games")"
 #2.13
 
 echo -e "\nAverage number of goals in all games from both teams:"
-echo "$($PSQL "SELECT AVG(winner_goals)+AVG(opponent_goals) FROM games")"
+echo "$($PSQL "SELECT ROUND(AVG(winner_goals)+AVG(opponent_goals),16) FROM games")"
 #2.8125000000...
 
 echo -e "\nMost goals scored in a single game by one team:"
